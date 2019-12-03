1fe1863a3f118bbacd7e33100331c0b0e66ac432
AbhishekEluri <abhishekeluri@gmail.com>
2019-12-03 19:36:48 +0530

    commit practise

---
+++ MyProject/src/MyClass.java
@@ -0,0 +1,12 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int MyBankAccountNumber;
+		String MyBankName;
+		System.out.println("hello world");
+		
+	}
+
+}
